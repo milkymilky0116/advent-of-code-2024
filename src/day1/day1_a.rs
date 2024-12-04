f3c882e2d549c449d00a0d47a05c2e5ebace89d6
milkymilky0116 <sjlee990129@gmail.com>
2024-12-04 10:19:51 +0900

    chore: improve day1_a

--- src/day1/day1_a.rs
+++ src/day1/day1_a.rs
@@ -3,35 +3,22 @@ use std::fs;
 pub fn day1_a(file_name: String) -> i32 {
     let mut left_nums = vec![];
     let mut right_nums = vec![];
-    fs::read_to_string(file_name)
-        .expect("Fail to read file")
-        .lines()
-        .map(|line| line.split_whitespace().collect::<Vec<&str>>())
-        .collect::<Vec<Vec<&str>>>()
+    let file_content = fs::read_to_string(file_name).expect("Fail to read file");
+    file_content.lines().for_each(|line| {
+        let nums: Vec<i32> = line
+            .split_whitespace()
+            .map(|value| value.parse::<i32>().expect("Fail to parse to i32"))
+            .collect();
+        if let [left, right] = nums.as_slice() {
+            left_nums.push(*left);
+            right_nums.push(*right);
+        }
+    });
+    left_nums.sort_unstable();
+    right_nums.sort_unstable();
+    left_nums
         .iter()
-        .for_each(|arr| {
-            let left_val = arr
-                .get(0)
-                .expect("Fail to get left value")
-                .parse::<i32>()
-                .expect("Fail to cast to i64");
-
-            let right_val = arr
-                .get(1)
-                .expect("Fail to get left value")
-                .parse::<i32>()
-                .expect("Fail to cast to i64");
-            left_nums.push(left_val);
-            right_nums.push(right_val);
-            left_nums.sort();
-            right_nums.sort();
-        });
-    let mut result = 0;
-    for i in 0..left_nums.len() {
-        let left_small_val = left_nums.get(i).expect("Fail to get left array value");
-
-        let right_small_val = right_nums.get(i).expect("Fail to get left array value");
-        result += (left_small_val - right_small_val).abs();
-    }
-    result
+        .zip(right_nums.iter())
+        .map(|(left, right)| (left - right).abs())
+        .sum()
 }
