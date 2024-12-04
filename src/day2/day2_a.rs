0c8ad076647d5bbe2120e6c2a8ca091428c194f7
milkymilky0116 <sjlee990129@gmail.com>
2024-12-04 11:16:24 +0900

    feat: complete day2_a

---
+++ src/day2/day2_a.rs
@@ -0,0 +1,33 @@
+use std::fs;
+
+pub fn day2_b(filename: String) -> i32 {
+    let file_content = fs::read_to_string(filename).expect("Fail to read file");
+    let lines: Vec<Vec<i32>> = file_content
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|value| value.parse::<i32>().expect("Fail to parse str to i32"))
+                .collect::<Vec<i32>>()
+        })
+        .collect();
+    lines
+        .iter()
+        .filter(|line| {
+            let mut has_increase = false;
+            let mut has_decrease = false;
+            for window in line.windows(2) {
+                let (left_val, right_val) = (window[0], window[1]);
+                let diff = (left_val - right_val).abs();
+                if diff > 3 || diff == 0 || (has_increase && has_decrease) {
+                    return false;
+                }
+                if left_val < right_val {
+                    has_increase = true;
+                } else {
+                    has_decrease = true;
+                }
+            }
+            true
+        })
+        .count() as i32
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use advent_of_code_2024::day1::day1_b::day1_b;
+use advent_of_code_2024::day2::day2_a::day2_b;
 
 fn main() {
-    println!("{}", day1_b("inputs/day1_b.txt".to_string()));
+    println!("{}", day2_b("inputs/day2_a_test.txt".to_string()));
 }
